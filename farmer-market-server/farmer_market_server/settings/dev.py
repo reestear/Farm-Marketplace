da08228387dd94be4fdfac74a735159cd0b4addb
ruslan tasmukhanov <ruslahn.q@gmail.com>
2024-10-12 14:40:50 +0500

    [add]: postgres configs

--- farmer-market-server/farmer_market_server/settings/dev.py
+++ farmer-market-server/farmer_market_server/settings/dev.py
@@ -13,8 +13,15 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "database" / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "database" / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("DJANGO_POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+        "TEST": {"NAME": BASE_DIR / "test_farmer_market_db.sqlite3"},
     }
 }
 
