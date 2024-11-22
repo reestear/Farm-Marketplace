1232c4e507382a6c24111a46d1405414382f16fb
ruslan tasmukhanov <ruslahn.q@gmail.com>
2024-11-22 19:46:11 +0500

    [fix]: entrypoint

--- farmer-market-server/.build/entrypoint.sh
+++ farmer-market-server/.build/entrypoint.sh
@@ -6,7 +6,7 @@ python manage.py migrate --no-input
 
 # DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 
-if [[ $DEBUG -eq 0 ]]; then
+if [[ "$DEBUG" == "0" ]]; then
 # production mode
 #   daphne -b 0.0.0.0 -p 8010 sfb_market_backend.asgi:application &
   gunicorn farmer_market_server.wsgi:application --bind 0.0.0.0:8000
