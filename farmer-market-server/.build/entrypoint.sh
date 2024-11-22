3bb20a5178ac8ee30259339ba3bd6c5de93ec79f
ruslan tasmukhanov <ruslahn.q@gmail.com>
2024-11-22 19:21:02 +0500

    [edit: compose not necessary, dockerfile] [add: static files configs]

--- farmer-market-server/.build/entrypoint.sh
+++ farmer-market-server/.build/entrypoint.sh
@@ -2,7 +2,7 @@
 
 python manage.py migrate --no-input
 
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
 
 # DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 
