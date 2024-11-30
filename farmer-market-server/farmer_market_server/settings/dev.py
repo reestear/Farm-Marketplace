90777bdf6319580041dd66a92ea0c822bdd5f4d0
ruslan tasmukhanov <ruslahn.q@gmail.com>
2024-11-30 23:49:24 +0500

    [add: dev]

--- farmer-market-server/farmer_market_server/settings/dev.py
+++ farmer-market-server/farmer_market_server/settings/dev.py
@@ -1,5 +1,7 @@
 import os
 
+import dj_database_url
+
 from .base import *
 
 DEBUG = True
@@ -7,19 +9,22 @@ ALLOWED_HOSTS = ["*"]
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-DATABASES = {
-    "default": {
-        # "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": BASE_DIR / "database" / "db.sqlite3",
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("DJANGO_POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
-        "TEST": {"NAME": BASE_DIR / "test_farmer_market_db.sqlite3"},
+if os.getenv("DEV_USE_DB") == "TRUE":
+    DATABASES = {"default": dj_database_url.config(default=os.getenv("POSTGRES_URL"))}
+else:
+    DATABASES = {
+        "default": {
+            # "ENGINE": "django.db.backends.sqlite3",
+            # "NAME": BASE_DIR / "database" / "db.sqlite3",
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB"),
+            "USER": os.getenv("POSTGRES_USER"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+            "HOST": os.getenv("DJANGO_POSTGRES_HOST"),
+            "PORT": os.getenv("POSTGRES_PORT"),
+            "TEST": {"NAME": BASE_DIR / "test_farmer_market_db.sqlite3"},
+        }
     }
-}
 
 MEDIA_ROOT = BASE_DIR / "media"  # store locally
 MEDIA_URL = "/api/media/"
