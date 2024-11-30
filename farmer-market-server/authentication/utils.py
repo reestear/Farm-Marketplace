556f3a3c9946511d7a6b7d453af8230fa9107567
ruslan tasmukhanov <ruslahn.q@gmail.com>
2024-11-30 19:39:18 +0500

    [fix: redirect url after password reset]

--- farmer-market-server/authentication/utils.py
+++ farmer-market-server/authentication/utils.py
@@ -15,3 +15,18 @@ def replace_url_domain(url: str) -> str:
         str: The modified URL with the replaced word.
     """
     return re.sub(r"//([^/]+)/", f"//{settings.APPLICATION_HOST}/", url)
+
+
+def extract_uid_and_token(url):
+    # Regular expression to capture the UID and token in the URL
+    pattern = (
+        r"/password/reset/confirm/(?P<uid>[a-zA-Z0-9]+)/(?P<token>[a-zA-Z0-9\-]+)/?$"
+    )
+    match = re.search(pattern, url)
+
+    if match:
+        uid = match.group("uid")
+        token = match.group("token")
+        return uid, token
+    else:
+        raise ValueError("Invalid URL format or unable to extract UID and token.")
